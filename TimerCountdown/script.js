ebd214f4a2fc32ad46c49ecfc5a5a8b28910a837
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-05-04 11:18:53 +0100

    modified:   TimerCountdown/index.html 	modified:   TimerCountdown/script.js 	modified:   TimerCountdown/style.css

--- TimerCountdown/script.js
+++ TimerCountdown/script.js
@@ -1,28 +1,55 @@
-// Set the date we're counting down to
-const countDownDate = new Date("May 31, 2024 00:00:00").getTime();
-
-// Update the countdown every second
-const x = setInterval(function() {
-
-  // Get the current date and time
-  const now = new Date().getTime();
-
-  // Calculate the time remaining
-  const distance = countDownDate - now;
-
-  // Calculate days, hours, minutes, and seconds
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="timer"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the countdown is over, display a message
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+function setMinDateTime() {
+    const now = new Date().toISOString().slice(0, 16);
+    document.getElementById("setTimerD").min = now; 
   }
-}, 1000);
+  
+
+  window.addEventListener("load", setMinDateTime);
+  
+const form = document.querySelector(".form");
+
+
+const day = document.querySelector(".day>.value")
+const hour = document.querySelector(".hour>.value")
+const minutes = document.querySelector(".minutes>.value")
+const seconds = document.querySelector(".seconds>.value")
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const now = new Date().getTime();
+    const newT = form.setTimerD.value;
+    const newTimer = new Date(newT).getTime();
+  
+    function updateTimer() {
+      const now = new Date().getTime();
+      const distance = newTimer - now;
+  
+      if (distance <= 0) {
+        // If distance is negative or zero, stop the countdown and display a message
+        clearInterval(timerInterval);
+        day.innerHTML = '00';
+        hour.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';
+        return;
+      }
+  
+      const d = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const s = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      day.innerHTML = d;
+      hour.innerHTML = h;
+      minutes.innerHTML = m;
+      seconds.innerHTML = s;
+    }
+  
+    updateTimer();
+  
+    // Update every second
+    const timerInterval = setInterval(updateTimer, 1000);
+  });
+  
